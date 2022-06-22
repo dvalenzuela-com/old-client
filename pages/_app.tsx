1715f7764f10a61b43572068668780491ea48383
Daniel Valenzuela <daniel.valenzuela@carbyte.de>
2022-06-22 23:51:02 +0200

    adds firestore call to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CssBaseline } from '@mui/material'
 import { CartContext, CartProvider } from '../context/CartContext'
+import firestore from '../lib/firestore'
 
+
+// Call firestore to init it;
+firestore;
 
 
 function MyApp({ Component, pageProps }: AppProps) {
