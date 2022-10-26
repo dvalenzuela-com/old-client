c0ae95dd42e72c9f2f1208ad7f6232f20333420d
Daniel Valenzuela <daniel.valenzuela@carbyte.de>
2022-10-26 18:40:16 +0200

    adds NextSEO

--- pages/[business-id]/index.tsx
+++ pages/[business-id]/index.tsx
@@ -6,21 +6,25 @@ import { allProductsQuery, getAllBusinessIds, useProducts } from '@Lib/firestore
 import { useRouter } from 'next/router';
 import { useContext, useEffect } from 'react';
 import { CartContext, CartProvider } from '@Context/CartContext';
-import { VALID_BUSINESS_IDS } from '@Lib/siteConfig';
+import { GET_SITE_CONFIG, VALID_BUSINESS_IDS } from '@Lib/siteConfig';
 import Layout from 'layout/Layout';
 import { getDocs } from 'firebase/firestore';
 import { ABProduct } from '@dvalenzuela-com/alabarra-types';
+import { NextSeo } from 'next-seo';
+import { title } from 'process';
 
 const Index: NextPage<{products: ABProduct[]}> = ({products}) => {
 
 	const router = useRouter();
 	const businessId = router.query['business-id'] as string;
+    const SITE_CONFIG = GET_SITE_CONFIG(businessId);
 
 	// Testing SSR
 	//const [products, productsLoading, productsError, productsSnapshot] = useProducts(businessId);
 
 	const cart = useContext(CartContext);
 	const selectedTable = router.query['t'] as string;
+
 
 	useEffect(() => {
 		if (selectedTable && selectedTable.trim().length > 0) {
@@ -29,11 +33,21 @@ const Index: NextPage<{products: ABProduct[]}> = ({products}) => {
 	}, [selectedTable]);
 
   	return (
-		<Layout>
-			<Container>
-				<ProductGrid products={products}></ProductGrid>
-			</Container>
-		</Layout>
+		<>
+			<NextSeo
+				title={SITE_CONFIG.TITLE}
+				description={`Order and pay online from ${SITE_CONFIG.TITLE} and get your food & drinks super fast!`}
+				openGraph={{
+					title: SITE_CONFIG.TITLE,
+					description: `Order and pay online from ${SITE_CONFIG.TITLE} and get your food & drinks super fast!`,
+				}}
+			/>
+			<Layout>
+				<Container>
+					<ProductGrid products={products}></ProductGrid>
+				</Container>
+			</Layout>
+		</>
 
   	)
 }
